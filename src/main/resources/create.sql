9fa191448f7da81b585aac1b4c83983f9c2a9621
koujienami <k.enami@peacepiece.info>
2025-01-05 21:19:31 +0900

    AWS初級編用簡易アプリケーションの作成

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS 'student' (
-  'id' INT PRIMARY KEY AUTO_INCREMENT,
-  'name' VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS student (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL
 );
 
-INSERT INTO 'student' ('name') VALUES ('Kouji Enami');
+INSERT INTO student (name) VALUES ('Kouji Enami');
