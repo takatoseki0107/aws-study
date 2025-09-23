2ab768f3e0a842536880dd9c3719dd94f4148826
takatoseki0107 <majesty06270107@gmail.com>
2025-09-24 02:45:53 +0900

    fix: update terraform and ansible workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -136,7 +136,7 @@ resource "aws_security_group" "ec2" {
     protocol    = "tcp"
     from_port   = 22
     to_port     = 22
-    cidr_blocks = [var.allowed_ssh_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
