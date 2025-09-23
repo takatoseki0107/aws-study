d9c894b7a482417ace09d442e8502594bc2e0be4
takatoseki0107 <majesty06270107@gmail.com>
2025-09-24 06:46:03 +0900

    fix: restrict SSH ingress, secure SSH key handling

--- terraform/main.tf
+++ terraform/main.tf
@@ -140,7 +140,7 @@ resource "aws_security_group" "ec2" {
     protocol    = "tcp"
     from_port   = 22
     to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.allowed_ssh_cidr]
   }
 
   ingress {
