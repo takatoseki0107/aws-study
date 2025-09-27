c89270702fde56697234b030dc21016ade606aba
takatoseki0107 <majesty06270107@gmail.com>
2025-09-27 19:52:13 +0900

    fix: SSH鍵の設定を更新

--- terraform/main.tf
+++ terraform/main.tf
@@ -191,7 +191,7 @@ data "aws_ami" "al2" {
 resource "aws_instance" "web" {
   ami                         = data.aws_ami.al2.id
   instance_type               = "t2.micro"
-  key_name                    = var.key_name
+  key_name                    = "aws-study-key"
   subnet_id                   = aws_subnet.public_a.id
   vpc_security_group_ids      = [aws_security_group.ec2.id]
   associate_public_ip_address = true
